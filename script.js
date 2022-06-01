f203a9912abba60f1d9a9510e6b6f5ab76e22ce1
Jayden Griffith <griffithjayden97@outlook.com>
2022-05-31 19:46:16 -0600

    Further aided readability. Added pseudo-code, and notes for development.

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ var question2 = document.getElementById("question-2")
 var question3 = document.getElementById("question-3")
 var question4 = document.getElementById("question-4")
 var question5 = document.getElementById("question-5")
+
+// Access different buttons by child index?
