2782c7acf901dc300e332d64a98a0293fbe8fce7
Jayden Griffith <griffithjayden97@outlook.com>
2022-05-31 19:32:40 -0600

    Reoriented HTML for readability. Added some preliminary selectors to the script file.

---
+++ script.js
@@ -0,0 +1,5 @@
+var question1 = document.getElementById("question-1")
+var question2 = document.getElementById("question-2")
+var question3 = document.getElementById("question-3")
+var question4 = document.getElementById("question-4")
+var question5 = document.getElementById("question-5")
