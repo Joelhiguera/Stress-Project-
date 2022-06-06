757233994a722a225532d2bd3203f7923bce8c7d
Holden A <holdenatkinson@gmail.com>
2022-06-05 20:12:41 -0600

    added google maps API with functionality

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ var question5 = document.getElementById("question-5")
 // Will eventually need at least two fetch requests using different APIs
 // How will we recognize quiz 'score'?
 
+
+
+
+
+      function initMap() {
+        // The location of Uluru
+        const utah = {lat:40.433913164329596, lng:-111.87754869011466}
+        // The map, centered at Uluru
+        const map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 8,
+          center: utah,
+        });
+        // The marker, positioned at 
+        new google.maps.Marker({
+          position: utah,
+          map: map,
+        });
+
+      new google.maps.Marker({
+            position: {lat:40.133, lng:-111.87754869011466},
+            map: map,
+          });
+      }
+
+      
+      
+      window.initMap = initMap;
+      
+
+
